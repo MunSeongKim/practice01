e47793b4ad18f62712a796dac53809d348fc1eed
bit <bit@DESKTOP-518UE4O>
2018-02-19 16:31:12 +0900

    p3 complete

--- src/practice01/Prob03.java
+++ src/practice01/Prob03.java
@@ -1,9 +1,28 @@
 package practice01;
 
+import java.util.Scanner;
+
 public class Prob03 {
 
 	public static void main(String[] args) {
-
+		Scanner scanner = new Scanner( System.in );
+		
+		System.out.print("숫자를 입력하세요: ");
+		int number = scanner.nextInt();
+		int result = 0;
+		
+		if((number % 2) == 0) {
+			for(int i = 0; i <= number; i += 2) {
+				result += i;
+			}
+		} else {
+			for(int i = 1; i <= number; i += 2) {
+				result += i;
+			}
+						
+		}
+		System.out.println("결과 값: " + result);
+		
 	}
 
 }
